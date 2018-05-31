8a6a6d60913f6243f2eb1defa1c59e56d87a7c94
sheldonhh <vagrant@homestead>
2018-05-31 03:27:43 +0000

    remove diffForHumans

--- resources/views/users/_topics.blade.php
+++ resources/views/users/_topics.blade.php
@@ -10,7 +10,7 @@
                 <span class="meta pull-right">
                 {{ $topic->reply_count }} 回复
                 <span> ⋅ </span>
-                    {{ $topic->created_at->diffForHumans() }}
+                    <!-- {{ $topic->created_at->diffForHumans() }} -->
             </span>
             </li>
         @endforeach
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('users', 'UsersController',['only' => ['show', 'update', 'edit']]);
 //Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 Auth::routes();
