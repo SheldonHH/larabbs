d034b984487d657acbc629846b24f545a12f514a
sheldonhh <vagrant@homestead>
2018-05-31 02:20:39 +0000

    Initial commit

---
+++ app/Models/Topic.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+class Topic extends Model
+{
+    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    // laravel的本地作用域
+    public function scopeWithOrder($query, $order){
+        switch($order){
+            case 'recent':
+                $query->recent();
+                break;
+            default:
+                $query->recentReplied();
+                break;
+        }
+        return $query->with('user', 'category');
+    }
+    // 预加载 防止 N+1 问题
+
+    public function scopeRecentReplied($query){
+        // for new ply, we will write logic, 更新话题 reply_count
+        // trigger tthe data_model, updated_at timestamp
+        return $query->orderBy('updated_at', 'desc');
+}
+
+    public function scopeRecent($query){
+        // orderBy
+        return $query->orderBy('created_at', 'desc');
+    }
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
+    <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('styles')
+</head>
+
+<body>
+<div id="app" class="{{ route_class() }}-page">
+<!--自定义辅助方法：route_class()  -->
+    @include('layouts._header')
+
+    <div class="container">
+      <!-- 导航区加入子模板 -->
+        @include('layouts._message')
+        @yield('content')
+        <!--允许继承该模板，并注入内容  -->
+    </div>
+
+    @include('layouts._footer')
+</div>
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+@yield('scripts')
+</body>
+</html>
